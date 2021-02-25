05cf4ff355f9c12c8709ccd2a0b0f306a52149cd
gokullakshmi123 <gokula.scorpio@gmail.com>
2021-02-25 15:41:34 +0530

    class

---
+++ ASsignment/Books.swift
@@ -0,0 +1,16 @@
+//
+//  books.swift
+//  ASsignment
+//
+//  Created by VENKATARAMA LAKAMRAJU on 2/22/21.
+//  Copyright © 2021 VENKATARAMA LAKAMRAJU. All rights reserved.
+//
+
+import Foundation
+
+struct Books:Codable
+{
+   let BookID: String
+   let BookName: String
+   let BookDesc: String
+}
